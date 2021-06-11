ece6bc2360f436f36cf868ffe70ce330a98f16fa
NicoleOdo <nicole.odo@unb.ca>
2021-06-11 09:48:10 -0300

    Assignment 4

--- Assignment 04.R
+++ Assignment 04.R
@@ -2,73 +2,73 @@
 library(shiny)
 library(tidyverse)
 
-# Data Connection and Renaming Column ----
+# Data Connection and Renaming Column
 
 TestResultData <- read_csv("MathTestResults.csv") %>%
   rename(
     LevelOnePercent = "Level 1 %"
   )
 
-# Define UI for level 1 scores app ----
+# Define UI for level 1 scores
 ui <- fluidPage(
   
-  # App title ----
+  # App title
   titlePanel("Level 1 Math Test Results Obtained in New York State"),
   
-  # Sidebar layout with input and output definitions ----
+  # Sidebar layout with input and output definitions
   sidebarLayout(
     
-    # Sidebar panel for inputs ----
+    # Sidebar panel for inputs
     sidebarPanel(
       
-      # Input: Selector for variable to plot against level 1 test results ----
+      # Input: Selector for variable to plot against level 1 test results
       selectInput("variable", "Variable:",
                   c("Grade" = "Grade",
                     "Year" = "Year",
                     "Category" = "Category")),
       
-      # Input: Checkbox for whether outliers should be included ----
+      # Input: Checkbox for whether outliers should be included
       checkboxInput("outliers", "Show outliers", TRUE)
       
     ),
     
-    # Main panel for displaying outputs ----
+    # Main panel for displaying outputs
     mainPanel(
       
-      # Description ----
+      # Description
       h5("This app allows us to investigate the New York State Mathematics Tests results from 
       years 2006 to 2011. Here we are specifically interested in understanding the percent
       of students that obtained a Level 1 test result, and further explore the results by
       observing by grade level, year and category of student."),
       
-      # Data set location ----
+      # Data set location
       h5("To view the complete dataset, you can visit the following link:
          https://catalog.data.gov/dataset/2006-2011-nys-math-test-results-by-grade-citywide-by-race-ethnicity"),
       
-      # Output: Formatted text for caption ----
+      # Output: Formatted text for caption
       h3(textOutput("caption")),
       
-      # Output: Plot of the requested variable against level 1 test results ----
+      # Output: Plot of the requested variable against level 1 test results
       plotOutput("testPlot")
       
     )
   )
 )
 
-# Define server logic to plot various variables against level 1 test results ----
+# Define server logic to plot various variables against level 1 test results
 server <- function(input, output) {
   
-  # Compute the formula text ----
+  # Compute the formula text
   formulaText <- reactive({
     paste("LevelOnePercent ~", input$variable)
   })
   
-  # Return the formula text for printing as a caption ----
+  # Return the formula text for printing as a caption
   output$caption <- renderText({
     formulaText()
   })
   
-  # Generate a plot of the requested variable against level 1 test results ----
+  # Generate a plot of the requested variable against level 1 test results
   # and only exclude outliers if requested
   output$testPlot <- renderPlot({
     boxplot(as.formula(formulaText()),
@@ -80,5 +80,5 @@ server <- function(input, output) {
   
 }
 
-# Create Shiny app ----
+# Create Shiny app
 shinyApp(ui, server)
